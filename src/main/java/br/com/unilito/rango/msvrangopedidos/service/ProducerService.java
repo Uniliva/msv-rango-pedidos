9f10c9f089e919ddcd199e6a65d871f4014e2b88
Weliton Martins <weliton.martins@yaman.com>
2020-09-16 00:29:09 -0300

    modificando topico

--- src/main/java/br/com/unilito/rango/msvrangopedidos/service/ProducerService.java
+++ src/main/java/br/com/unilito/rango/msvrangopedidos/service/ProducerService.java
@@ -1,6 +1,7 @@
 package br.com.unilito.rango.msvrangopedidos.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +11,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ProducerService {
 
+	@Value("${topic.kafka.pedido}")
+	private String topico;
+
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
 
 	public void sendMessage(String message) {
-		log.info("Postando mensagem no topico: TOPICO_PEDIDO");
-		this.kafkaTemplate.send("TOPICO_PEDIDO", message);
+		log.info("Postando mensagem no topico: {}", topico);
+		this.kafkaTemplate.send(topico, message);
 	}
 
 }
